a615ad9d2b18862fb98ae06565cd38b0db29e222
Kosta Kazakov <kostakazakoff@gmail.com>
2022-05-26 17:40:35 +0300

    Update find_files_by_extension.py
    Finding a files by extension in the chosen system path and print their names and total size in MB

--- find_files_by_extension.py
+++ find_files_by_extension.py
@@ -7,13 +7,18 @@ system_path = input('Enter/the/path/in/system: ')
 def find_file_by_extension(extension, path):
     size = 0
     output = ''
+    # creating a list for searched files:
     file_list = []
     for root, dirs, files in os.walk(path):
         for searched_file in files:
+            # checking if the extension of the file is the searched one:
             if searched_file.endswith(extension):
+                # append the list of results:
                 file_list.append(os.path.join(root, searched_file))
+    # preparing the output:
     for each in file_list:
         output += str(each) + '\n'
+        # taking the size of the current file and reducing the total files size:
         size += int(os.path.getsize(each))
     output += 'Total Size: ' + str(size // (1024*1024)) + ' MB'
     return output
